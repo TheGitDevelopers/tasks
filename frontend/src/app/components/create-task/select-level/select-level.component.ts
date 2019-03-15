aecb81d697a3f4ac69f5d8f1018341dc7013fb97
hubert.wozniak977@gmail.com <hubert.wozniak977@gmail.com>
2019-03-15 13:48:11 +0100

    end-commit

--- frontend/src/app/components/create-task/select-level/select-level.component.ts
+++ frontend/src/app/components/create-task/select-level/select-level.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-select-level',
   templateUrl: './select-level.component.html',
-  styleUrls: ['./select-level.component.css']
+  styleUrls: ['./select-level.component.css','./select-level-rwd.component.css']
 })
 export class SelectLevelComponent implements OnInit {
   constructor() {}
